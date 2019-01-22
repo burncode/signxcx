a24fe9472143bb915a2b55c6d2041760e7bfe26f
yizenghui <zenghuitrue@gmail.com>
2019-01-22 23:39:33 +0800

    save

---
+++ src/api/comm.js
@@ -0,0 +1,15 @@
+/* eslint-disable padded-blocks */
+import base from './base'
+import wepy from 'wepy'
+
+/**
+ * 权限服务类
+ */
+export default class comm extends base {
+
+  static async getUser(id) {
+    const url = `${this.baseUrl}/api/user/${id}`
+    return await this.get(url)
+  }
+
+}
